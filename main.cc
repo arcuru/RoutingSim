d78ba0678062cb79e6b6c07e6c727625d596d6db
Patrick Jackson <PatrickSJackson@gmail.com>
2012-04-30 18:20:28 -0400

    Setup command line inputs for types of networks.

--- main.cc
+++ main.cc
@@ -17,6 +17,7 @@ uint64_t eibuf_util = 0;
 uint64_t aobuf_util = 0;
 uint64_t eobuf_util = 0;
 void RunSimulation( uint32_t simulation_end, double injection_chance );
+void SetupMemCont( uint32_t type );
 
 int main ( int argc, char** argv )
 {
@@ -26,15 +27,7 @@ int main ( int argc, char** argv )
 	// Initialize default network settings
 	NInfo.width = 8;
 	NInfo.height = 8;
-	NInfo.dest_func = RAND;
-	MC[0].x = 0; MC[0].y = 0;
-	MC[1].x = 1; MC[1].y = 1;
-	MC[2].x = 2; MC[2].y = 2;
-	MC[3].x = 3; MC[3].y = 3;
-	MC[4].x = 4; MC[4].y = 4;
-	MC[5].x = 5; MC[5].y = 5;
-	MC[6].x = 6; MC[6].y = 6;
-	MC[7].x = 7; MC[7].y = 7;
+	NInfo.dest_func = MEM_CONT;
 
 	// Generate headers for the output
 	cout << "Packet Injections, ";
@@ -50,7 +43,7 @@ int main ( int argc, char** argv )
 	cout << "Simulation Time, ";
 	cout << "Injection Chance" << endl;
 	if (argc > 1) {
-		NInfo.dest_func = (Destination_Function)atoi(argv[1]);
+		SetupMemCont( atoi(argv[1]) );
 		if (argc > 2) {
 			simulation_end = atoi(argv[2]);
 			if (argc > 3) {
@@ -88,8 +81,8 @@ void RunSimulation( uint32_t simulation_end, double injection_chance )
 	//srand(time(NULL));
 	
 	// Set injection chance
-	NInfo.chance = injection_chance / 12;
-	NInfo.adaptive = true;
+	NInfo.chance = injection_chance / 100;
+	NInfo.adaptive = false;
 
 	// Create Router and packet generators for request network
 	NArray = new Router[NInfo.width * NInfo.height];
@@ -133,7 +126,7 @@ void RunSimulation( uint32_t simulation_end, double injection_chance )
 	cout << packet_injections << ", ";
 	cout << packet_ejections << ", ";
 	cout << ((double)packet_injections)/simulation_end << ", ";
-	cout << ((16 * (double)packet_ejections)/(NInfo.width*NInfo.height))/simulation_end << ", ";
+	cout << ((17 * (double)packet_ejections)/8)/simulation_end << ", ";
 	cout << (double)packet_latency / packet_ejections << ", ";
 	cout << (double)link_util / (NInfo.width * NInfo.height * 4 * simulation_end) << ", ";
 	cout << (double)aibuf_util / (NInfo.width * NInfo.height * 4 * 4 * simulation_end) << ", ";
@@ -159,3 +152,90 @@ void RunSimulation( uint32_t simulation_end, double injection_chance )
 
 	return ;
 }
+
+/** SetupMemCont
+ *  initializes the locations of the memory controllers in the network based on the input
+ *
+ *  @arg type Specifies the setup of the network
+ */
+void SetupMemCont ( uint32_t type )
+{
+	switch ( type ) {
+		case 1:	
+			// Random placement
+	MC[0].x = 0; MC[0].y = 0;
+	MC[1].x = 1; MC[1].y = 0;
+	MC[2].x = 2; MC[2].y = 0;
+	MC[3].x = 3; MC[3].y = 0;
+	MC[4].x = 4; MC[4].y = 0;
+	MC[5].x = 5; MC[5].y = 0;
+	MC[6].x = 6; MC[6].y = 0;
+	MC[7].x = 7; MC[7].y = 0;
+			break;
+
+		case 2:	
+			// Single Row
+			MC[0].x = 0; MC[0].y = 4;
+			MC[1].x = 1; MC[1].y = 4;
+			MC[2].x = 2; MC[2].y = 4;
+			MC[3].x = 3; MC[3].y = 4;
+			MC[4].x = 4; MC[4].y = 4;
+			MC[5].x = 5; MC[5].y = 4;
+			MC[6].x = 6; MC[6].y = 4;
+			MC[7].x = 7; MC[7].y = 4;
+			break;
+
+		case 3:	
+			// Single Col
+			MC[0].x = 4; MC[0].y = 0;
+			MC[1].x = 4; MC[1].y = 1;
+			MC[2].x = 4; MC[2].y = 2;
+			MC[3].x = 4; MC[3].y = 3;
+			MC[4].x = 4; MC[4].y = 4;
+			MC[5].x = 4; MC[5].y = 5;
+			MC[6].x = 4; MC[6].y = 6;
+			MC[7].x = 4; MC[7].y = 7;
+			break;
+
+		case 4:	
+			// Diagonal
+			MC[0].x = 0; MC[0].y = 0;
+			MC[1].x = 1; MC[1].y = 1;
+			MC[2].x = 2; MC[2].y = 2;
+			MC[3].x = 3; MC[3].y = 3;
+			MC[4].x = 4; MC[4].y = 4;
+			MC[5].x = 5; MC[5].y = 5;
+			MC[6].x = 6; MC[6].y = 6;
+			MC[7].x = 7; MC[7].y = 7;
+			break;
+
+		case 5:	
+			// Split Diagonal
+			MC[0].x = 0; MC[0].y = 0;
+			MC[1].x = 1; MC[1].y = 1;
+			MC[2].x = 2; MC[2].y = 5;
+			MC[3].x = 3; MC[3].y = 4;
+			MC[4].x = 4; MC[4].y = 3;
+			MC[5].x = 5; MC[5].y = 2;
+			MC[6].x = 6; MC[6].y = 6;
+			MC[7].x = 7; MC[7].y = 7;
+			break;
+
+		case 6:	
+			// Unique Row, Column, and Diagonal
+	MC[0].x = 0; MC[0].y = 0;
+	MC[1].x = 1; MC[1].y = 0;
+	MC[2].x = 2; MC[2].y = 0;
+	MC[3].x = 3; MC[3].y = 0;
+	MC[4].x = 4; MC[4].y = 0;
+	MC[5].x = 5; MC[5].y = 0;
+	MC[6].x = 6; MC[6].y = 0;
+	MC[7].x = 7; MC[7].y = 0;
+			break;
+
+		default:	
+			printf("Invalid configuration setup.\n");
+			break;
+	}
+}
+
